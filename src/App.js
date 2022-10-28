60505af8978016e1473f6d5fd26ff97602502700
Abu Bakar <76771529+Abu0978123@users.noreply.github.com>
2022-10-29 00:50:48 +0500

    outed some error

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 // import { createContext } from 'react';
-import TabPanel from "./CustomStyle";
+// import TabPanel from "./CustomStyle";
 import "./App.css";
 import React from "react";
-import Axios from "./Axios";
-import TodoList from "./Todo_project/TodoList";
+// import Axios from "./Axios";
+// import TodoList from "./Todo_project/TodoList";
 import "bootstrap/dist/css/bootstrap.min.css";
 import IndexPage1 from "./CRUD-2-project/IndexPageCRUD2";
-import IndexPage2 from "./shoping-cart/IndexPageShoping";
-import IndexPage3 from "./crud project/IndexPageCRUD";
-import Form from "./Registeration-Project/Form";
-import Project from "./Bootstrap-project/Project";
-import PropTypee from "./PropType/PropTypee";
+// import IndexPage2 from "./shoping-cart/IndexPageShoping";
+// import IndexPage3 from "./crud project/IndexPageCRUD";
+// import Form from "./Registeration-Project/Form";
+// import Project from "./Bootstrap-project/Project";
+// import PropTypee from "./PropType/PropTypee";
 function App() {
   return (
     <>
-      <TabPanel/>
-    <Axios/> 
-   <TodoList/>
-    <Project/>
-    <PropTypee />
+//       <TabPanel/>
+//     <Axios/> 
+//    <TodoList/>
+//     <Project/>
+//     <PropTypee />
     <IndexPage1/>
-    <Form/>
-    <IndexPage2/>
+//     <Form/>
+//     <IndexPage2/>
     
-     <IndexPage3/>
+//      <IndexPage3/>
 
     </>
   );
